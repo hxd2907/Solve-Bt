7b839f4305087969aecfbdc99a9cd116a27e9a9d
hxd2907 <hoxuandung1010@gmail.com>
2025-11-28 09:11:33 +0700

    Update index.tsx

--- scr/index.tsx
+++ scr/index.tsx
@@ -1,1 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SolutionView from "./components/SolutionView";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SolutionView />
+  </React.StrictMode>
+);
 
